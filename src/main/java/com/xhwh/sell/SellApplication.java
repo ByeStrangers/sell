b656dd9f830f097923531936bfa42e74ede09e5b
ByeStrangers <18720920393@163.com>
2019-11-15 22:56:49 +0800

    1、整合mybatis 和 对应分页插件

--- src/main/java/com/xhwh/sell/SellApplication.java
+++ src/main/java/com/xhwh/sell/SellApplication.java
@@ -1,9 +1,12 @@
 package com.xhwh.sell;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@MapperScan("com.xhwh.sell.mapper")
 public class SellApplication {
 
     public static void main(String[] args) {
