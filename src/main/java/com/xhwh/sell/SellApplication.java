3e996e6793b590c004f165cd40ec022cce2c7fe9
ByeStrangers <18720920393@163.com>
2021-01-16 22:26:44 +0800

    1、整合layui mini

--- src/main/java/com/xhwh/sell/SellApplication.java
+++ src/main/java/com/xhwh/sell/SellApplication.java
@@ -4,13 +4,26 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.xhwh.sell.mapper")
+@Controller
 public class SellApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/page/{path}")
+    public String path(@PathVariable("path") String path) {
+        return String.format("page/%s", path);
+    }
 }
